76532a8c2b4386e83375dd235fe491f2e37a7197
re0phimes <re0phimes@outlook.com>
2024-11-07 02:34:25 +0000

    feat:放宽ts配置

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,11 @@ import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve:{
+    alias:{
+      '@': '/src'
+      }
+    },
   css: {
     postcss: {
       plugins: [
